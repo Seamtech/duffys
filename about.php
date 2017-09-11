cfb4fdc484081e3de50f919f0f466c2098855b6e
Seamtech <31858002+Seamtech@users.noreply.github.com>
2017-09-11 10:55:08 -0500

    Initial Upload

---
+++ about.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<link href="includes/main.css" rel="stylesheet" type="text/css">
+<?php include ('includes/header.html');?>
+</head>
+
+<body>
+<div class="main">
+    <image height="450px" width="757.5px" src="/im/patio.png"</image>
+    <p>Content Area</p>
+    <p>Content Area</p>
+    </div>
+	<div class="container">
+    <div class="left">
+    <p>Title Area</p>
+    <hr class="hr1">
+    <p>Content Area</p>
+    <p>Content Area</p>
+    <p>Content Area</p>
+    
+    </div>
+	<div class="right">
+    <p>Title Area</p>
+    <hr class="hr1"></hr>
+    <p>Content Area</p>
+    <p>Content Area</p>
+    <p>Content Area</p>
+    </div>
+    </div>
+</body>
+</html>
